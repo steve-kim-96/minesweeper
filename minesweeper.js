21c9c89aaf4a52435c628311b8ce29450e7c7edc
steve-kim-96 <soungku@hotmail.com>
2020-11-25 15:00:50 +1300

    update for win condition

--- minesweeper.js
+++ minesweeper.js
@@ -19,13 +19,16 @@ var board = {
     { row: 3, col: 1, isMarked: false, isMine: true, hidden: true },
     { row: 3, col: 2, isMarked: false, isMine: false, hidden: true },
     { row: 3, col: 3, isMarked: false, isMine: true, hidden: true },
-],
+  ],
 };
 
 function startGame() {
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
+
   board.cells.forEach(
     cell => cell.surroundingMines = countSurroundingMines(cell)
-);
+  );
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -35,21 +38,27 @@ function startGame() {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin() {
-  //IF visible cells DO NOT EQUAL mines AND // OR I could do if all hidden cells EQUAL bombs
-  //all of the mines are marked
-  //THEN player wins and display win message
+  //LOOP through board.cells.board
+  //WIN condition is if all cells are revealed and all mines are marked
+  for (let i = 0; i < board.cells.length; i++) {
+    let boardcells = board.cells[i];
+    if(boardcells.isMine && !boardcells.isMarked){
+      return;
+    }
+    if (boardcells.isMine && boardcells.isMarked) {
+      lib.displayMessage('You win!')
+    };
+    // You can use this function call to declare a winner (once you've
+    // detected that they've won, that is!)
 
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  }
 }
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`: 
 //
-  
+
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
